adc9a17aeb3b32cb444615aebb993b361f637cd6
mdu25th <mdu25.th@gmail.com>
2026-04-26 20:58:01 +0700

    Add files via upload

---
+++ mymdu_app_v2/sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'mymdu-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
